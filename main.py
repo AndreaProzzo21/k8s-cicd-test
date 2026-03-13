7bb63699074ba7e82b738ee71417e16facf6455d
AndreaProzzo21 <and.prozzo@gmail.com>
2026-03-13 20:59:17 +0100

    Added manifest and fixed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/test')
 def hello():
-    return "<h1>Pipeline Funzionante! Let's GO!</h1><p>Versione 1.0</p>"
+    return "<h1>Pipeline Funzionante! Let's GOOOOO!</h1><p>Versione 1.0</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
