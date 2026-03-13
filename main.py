4386d11a767d9bbcf29d4a850f1b28deac20e54a
AndreaProzzo21 <and.prozzo@gmail.com>
2026-03-13 20:27:40 +0100

    Second push CICD test

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/test')
 def hello():
-    return "<h1>SGM-3007: Pipeline Funzionante!</h1><p>Versione 1.0</p>"
+    return "<h1>Pipeline Funzionante! Let's GO!</h1><p>Versione 1.0</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
